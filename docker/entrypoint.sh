1508fe3463d4c702cf43a262e02fa4389107a94a
Espen Hagen <2492641+espenhgn@users.noreply.github.com>
2024-10-27 22:53:44 +0100

    fix parsing of args, entrypoints

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -3,19 +3,39 @@ set -e
 
 case "$1" in
   "mix3r_int_weights")
-    /opt/conda/bin/python /scripts/mix3r_int_weights.py
+    shift
+    /opt/conda/bin/python /scripts/mix3r_int_weights.py "$@"
     ;;
   "extract_p")
-    /opt/conda/bin/python /scripts/extract_p.py
+    shift
+    /opt/conda/bin/python /scripts/extract_p.py "$@"
     ;;
   "make_template")
-    /opt/conda/bin/python /scripts/make_template.py
+    shift
+    /opt/conda/bin/python /scripts/make_template.py "$@"
     ;;
   "make_euler")
-    /opt/conda/bin/Rscript /scripts/make_euler.r
+    shift
+    /opt/conda/bin/Rscript /scripts/make_euler.r "$@"
+    ;;
+  "python")
+    shift
+    /opt/conda/bin/python "$@"
+    ;;
+  "ipython")
+    shift
+    /opt/conda/bin/ipython "$@"
+    ;;
+  "jupyter")
+    shift
+    /opt/conda/bin/jupyter "$@"
+    ;;
+  "bash")
+    shift
+    /usr/bin/bash "$@"
     ;;
   *)
-    echo "Invalid command. Available commands are: mix3r_int_weights, extract_p, make_template, make_euler"
+    echo "Invalid command. Available commands are: mix3r_int_weights, extract_p, make_template, make_euler, python, ipython, jypyter, bash"
     exit 1
     ;;
 esac
